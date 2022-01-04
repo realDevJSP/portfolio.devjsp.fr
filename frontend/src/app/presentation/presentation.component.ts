c20e46877bd50a0048cdfddfd90705a094b16d40
Julien Salgueiro Pereira <admin@devjsp.fr>
2022-01-04 16:23:10 +0100

    ajout premier test d'animation

--- frontend/src/app/presentation/presentation.component.ts
+++ frontend/src/app/presentation/presentation.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
 
+import { trigger, state, style, animate, transition } from '@angular/animations';
+
 @Component({
   selector: 'app-presentation',
   templateUrl: './presentation.component.html',
-  styleUrls: ['./presentation.component.scss']
+  styleUrls: ['./presentation.component.scss'],
+  animations: [
+    trigger('etatInitialisation', [
+      state('terminee', style({
+        'transform': 'translateY(0%)',
+        'opacity': '100%'
+      })),
+      state('nonDebutee', style({
+        'transform': 'translateY(200px)',
+        'opacity': '0%'
+      })),
+      transition('nonDebutee => terminee', animate('1000ms ease'))
+    ])
+  ]
 })
 export class PresentationComponent implements OnInit {
 
-  constructor() { }
+  initialisation!: String;
+
+  constructor() {
+  }
 
   ngOnInit(): void {
+    this.initialisation = "nonDebutee";
+  }
+
+  initialise(): void {
+    if(this.initialisation === 'nonDebutee'){
+      this.initialisation = 'terminee';
+    }
   }
 
 }
